d69b37cfe20e52317315ff10fdf7f922e83a8445
James Snelling <admin@sketchydesignanddevelopment.com>
2021-02-16 21:14:28 -0800

    Update MainFragment.kt
    Remove unnecessary imports

--- app/src/main/java/com/sketchydesignanddevelopment/spinthewheel/MainFragment.kt
+++ app/src/main/java/com/sketchydesignanddevelopment/spinthewheel/MainFragment.kt
@@ -1,15 +1,12 @@
 package com.sketchydesignanddevelopment.spinthewheel
 
 import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import java.util.jar.Manifest
 
 class MainFragment:Fragment() {
 

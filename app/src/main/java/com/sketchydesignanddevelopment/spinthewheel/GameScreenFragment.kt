79f38b7dd1fdced5ae828b223f27a35accd2e985
James Snelling <admin@sketchydesignanddevelopment.com>
2021-02-17 22:04:02 -0800

    working on animation
    Working on animation after Android Studio failure

--- app/src/main/java/com/sketchydesignanddevelopment/spinthewheel/GameScreenFragment.kt
+++ app/src/main/java/com/sketchydesignanddevelopment/spinthewheel/GameScreenFragment.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import okhttp3.*
 import java.io.IOException
 
+
 class GameScreenFragment: Fragment() {
 
     //library to make easy REST calls
     private val client = OkHttpClient()
+
+    //initializing imageViews
+    lateinit var wheel: ImageView
+    lateinit var spinner: ImageView
+    lateinit var spinButton: Button
 
     //new instance
     companion object{
@@ -24,9 +32,9 @@ class GameScreenFragment: Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_game_screen, container, false)
     }
@@ -35,27 +43,31 @@ class GameScreenFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //instantiate wheel, spinner, and buttons
-        val wheel = activity!!.findViewById<ImageView>(R.id.spinnyWheel)
-        val spinner = activity!!.findViewById<ImageView>(R.id.spinner)
-        val spinButton = activity!!.findViewById<Button>(R.id.spinButton)
+        wheel = activity!!.findViewById(R.id.spinnyWheel)
+        spinner = activity!!.findViewById(R.id.spinner)
+        spinButton = activity!!.findViewById(R.id.spinButton)
         spinButton.setOnClickListener{
-            wheel.animate()
-            spinner.animate()
+            startAnimation()
             spinWheel("http://mockbin.org/bin/539dc092-8367-414a-8892-ed3b2d666dbe")
         }
     }
 
     private fun spinWheel(url: String){
-
         val getSpinSpaces = Request.Builder().url(url).build()
-        client.newCall(getSpinSpaces).enqueue(object:Callback{
+        client.newCall(getSpinSpaces).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 println(getString(R.string.error))
+                println(e)
             }
 
             override fun onResponse(call: Call, response: Response) {
                 println(response)
             }
         })
+    }
+
+    private fun startAnimation(){
+        val rotation: Animation = AnimationUtils.loadAnimation(context, R.anim.clockwise_rotation)
+        wheel.startAnimation(rotation)
     }
 }
